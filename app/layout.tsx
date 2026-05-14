abf20ac82caca5776b640240074edeb04e94bf04
muhammadaffan <muhammadaffanpvt@gmail.com>
2026-05-14 15:27:50 +0530

    import route fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from '../components/Header.jsx'
+import Header from './components/Header.jsx'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ServiceCard from '../components/ServiceCard.jsx'
+import ServiceCard from './components/ServiceCard.jsx'
 import Link from "next/link.js";
 export default function Home() {
   return (
